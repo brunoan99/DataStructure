aae2f3f3e0f152b6ca2390f895ac5f7043c72382
Bruno Andrade <brunoan99@gmail.com>
2022-06-04 18:59:57 -0300

    test(Stack): ensure Stach.peek return undefined if no value is early provided to stack

--- stack/stack.spec.ts
+++ stack/stack.spec.ts
@@ -6,4 +6,10 @@ describe('Stack', () => {
     const returned = sut.push('any_value')
     expect(returned).toBe(undefined)
   })
+
+  test('Should method peek return undefined if no value are provided to Stack', () => {    
+    const sut = new Stack()
+    const returned = sut.peek()
+    expect(returned).toBe(undefined)
+  })
 })
