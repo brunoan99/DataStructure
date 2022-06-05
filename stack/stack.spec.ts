a04e66e2490fb7653f18ef3b91b91bddb1b79cc6
Bruno Andrade <brunoan99@gmail.com>
2022-06-05 07:13:38 -0300

    refactor: move sut creation to a factory helper method

--- stack/stack.spec.ts
+++ stack/stack.spec.ts
@@ -1,32 +1,36 @@
 import { Stack } from './Stack'
 
+const makeSut = (): Stack<unknown> => {
+  return new Stack()
+}
+
 describe('Stack', () => {
   test('Should method push have no return', () => {    
-    const sut = new Stack()
+    const sut = makeSut()
     const returned = sut.push('any_value')
     expect(returned).toBe(undefined)
   })
 
   test('Should method peek return undefined if no value are provided to Stack', () => {    
-    const sut = new Stack()
+    const sut = makeSut()
     const returned = sut.peek()
     expect(returned).toBe(undefined)
   })
 
   test('Should method pop return undefined if no value are provided to Stack', () => {    
-    const sut = new Stack()
+    const sut = makeSut()
     const returned = sut.pop()
     expect(returned).toBe(undefined)
   })
 
   test('Should method size return zero if no value are provided to Stack', () => {    
-    const sut = new Stack()
+    const sut = makeSut()
     const returned = sut.size()
     expect(returned).toBe(0)
   })
 
   test('Should method storeage return an empty list if no value are provided to Stack', () => {    
-    const sut = new Stack()
+    const sut = makeSut()
     const returned = sut.storage()
     expect(returned).toEqual([])
   })
