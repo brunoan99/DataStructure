0016d95bb43b16a7be1995c4b349730d463cd507
Bruno Andrade <brunoan99@gmail.com>
2022-06-05 11:24:13 -0300

    feat(LinkedStack): ensure LinkedStack.peek return undefined if no value are provided to Stack

--- stacks/linkedStack/linkedStack.ts
+++ stacks/linkedStack/linkedStack.ts
@@ -1,20 +1,30 @@
 import { IStack } from '../IStack'
+import { StackNode } from './stackNode'
 
 export class LinkedStack<V> implements IStack<V> {
+  head: StackNode<V> | null
+
+  constructor () {
+    this.head = null
+  }
+
   push (value: V): void {
     return
   }
+
+  peek (): V | undefined {
+    return this.head?.value
+  }
+
   pop (): V | undefined {
     throw new Error('Method not implemented.')
   }
+
   size (): number {
     throw new Error('Method not implemented.')
   }
-  peek (): V | undefined {
-    throw new Error('Method not implemented.')
-  }
+
   storage (): V[] {
     throw new Error('Method not implemented.')
   }
-
 }
