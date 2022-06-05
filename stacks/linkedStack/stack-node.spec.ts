6e0e7eb640c04bf15bef2f9cc1cfeae41b588aa6
Bruno Andrade <brunoan99@gmail.com>
2022-06-05 11:20:22 -0300

    test(StackNode): ensure StackNode store a next StackNode when provided

--- stacks/linkedStack/stack-node.spec.ts
+++ stacks/linkedStack/stack-node.spec.ts
@@ -10,4 +10,12 @@ describe('Stack Node', () => {
     const sut = new StackNode('any', null)
     expect(sut.value).toBe('any')
   })
+
+  test('Should StackNode store a next StackNode when provided', () => {
+    const anotherStackNode = new StackNode('other_value', null)
+    const sut = new StackNode('any', anotherStackNode)
+    expect(sut.next).toEqual(anotherStackNode)
+    expect(sut.next.value).toBe('other_value')
+    expect(sut.next.next).toBe(null)
+  })
 })
