0678bc373e96ef777537d61a3fbcd3073a504a55
Bruno Andrade <brunoan99@gmail.com>
2022-06-05 11:31:20 -0300

    refactor(LinkedStack): move sut creation to a helper factory method

--- stacks/linkedStack/linked-stack.spec.ts
+++ stacks/linkedStack/linked-stack.spec.ts
@@ -1,13 +1,15 @@
 import { LinkedStack } from './linkedStack'
 
+const makeSut = (): LinkedStack<unknown> => new LinkedStack()
+
 describe('Linked Stack', () => {
   test('Should method push have no return', () => {
-    const sut = new LinkedStack()
+    const sut = makeSut()
     expect(sut.push('any')).toBe(undefined)
   })
 
   test('Should method peek return undefined if no value are provided to Stack', () => {    
-    const sut = new LinkedStack()
+    const sut = makeSut()
     expect(sut.peek()).toBe(undefined)
   })
 })
