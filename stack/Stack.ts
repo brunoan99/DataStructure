c9d4b91bcc50e586863fa53493b803149b2cf08a
Bruno Andrade <brunoan99@gmail.com>
2022-06-05 09:45:26 -0300

    feat(Stack): ensure Stack.pop returns the last value provided in push method

--- stack/Stack.ts
+++ stack/Stack.ts
@@ -16,7 +16,7 @@ export class Stack<V> implements IStack<V> {
   }
 
   pop (): V | undefined {
-    return this.stack[this.stack.length]
+    return this.stack[this.stack.length - 1]
   }
 
   size (): number {
