82111ec616aaef080373e180b72ceec242f8c266
Bruno Andrade <brunoan99@gmail.com>
2022-06-06 18:21:31 -0300

    test(DoubleEndedQueue): ensure DoubleEndedQueue.dequeueBack have no return if no value was provided to DoubleEndedQueue

--- queue/doubleEndedQueue/DoubleEndedQueue.ts
+++ queue/doubleEndedQueue/DoubleEndedQueue.ts
@@ -1,10 +1,15 @@
+import { IDoubleEndedQueue } from '../IDoubleEndedQueue'
 import { Queue } from '../queue/Queue'
 
-export class DoubleEndedQueue<V> extends Queue<V> {
+export class DoubleEndedQueue<V> extends Queue<V> implements IDoubleEndedQueue<V> {
   constructor () {
     super()
   }
   enqueueFront (value: V): void {
     this.queue.unshift(value)
+  }
+
+  dequeueBack (): V | undefined {
+    return
   }
 }
