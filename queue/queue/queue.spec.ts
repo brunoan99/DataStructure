8b805ccfb2f3c62bfb0bd38b76a55b4c8e23682e
Bruno Andrade <brunoan99@gmail.com>
2022-06-06 15:58:25 -0300

    test(Queue): ensure Queue.dequeue have no return if no value was provided to enqueue

--- queue/queue/queue.spec.ts
+++ queue/queue/queue.spec.ts
@@ -5,4 +5,9 @@ describe('Queue', () => {
     const sut = new Queue()
     expect(sut.enqueue('any')).toBe(undefined)
   })
+
+  test('Should method dequeue return undefined if no value are provided to enqueue', () => {
+    const sut = new Queue()
+    expect(sut.dequeue()).toBe(undefined)
+  })
 })
