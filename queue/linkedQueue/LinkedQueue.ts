5d426dc3253f1790b10639cbe7dc86dd086986bd
Bruno Andrade <brunoan99@gmail.com>
2022-06-06 23:29:14 -0300

    feat(LinkedQueue): ensure LinkedQueue.dequeue return a value if it was provided to enqueue method

--- queue/linkedQueue/LinkedQueue.ts
+++ queue/linkedQueue/LinkedQueue.ts
@@ -1,11 +1,24 @@
 import { IQueue } from '../IQueue'
+import { QueueNode } from './QueueNode'
 
 export class LinkedQueue<V> implements IQueue<V> {
+  protected start: QueueNode<V> | null = null 
+
   enqueue (value: V): void {
-    return
+    if (this.start) {
+      let tempQueueNode = this.start
+      while (tempQueueNode?.next !== null ) {
+        tempQueueNode = tempQueueNode.next
+      }
+      tempQueueNode.next = new QueueNode(value)
+      return
+    }
+    this.start = new QueueNode(value)
   }
   dequeue (): V | undefined {
-    return
+    const queueNodeToDequeue = this.start?.value
+    this.start = this.start?.next ? this.start.next : null 
+    return queueNodeToDequeue
   }
   peek (): V | undefined {
     return
