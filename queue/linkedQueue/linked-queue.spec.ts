b6a180ae2854083e6daf91a346ecd408c61cfbfc
Bruno Andrade <brunoan99@gmail.com>
2022-06-06 21:58:18 -0300

    test(LinkedQueue): ensure LinkedQueue.dequeue have no return if value was not provided to enqueue

--- queue/linkedQueue/linked-queue.spec.ts
+++ queue/linkedQueue/linked-queue.spec.ts
@@ -12,4 +12,9 @@ describe('Linked Queue', () => {
     const sut = makeSut()
     expect(sut.peek()).toBeUndefined()
   })
+
+  test('Should dequeue return undevined if no value was provided to enqueue method', () => {
+    const sut = makeSut()
+    expect(sut.dequeue()).toBeUndefined()
+  })
 })
