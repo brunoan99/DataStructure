5fdc4024fec7d766bcfacd6d533ac2c708591a96
Bruno Andrade <brunoan99@gmail.com>
2022-06-14 13:58:15 -0300

    refactor(LinkedStack): simplify tests

--- stacks/linkedStack/linked-stack.spec.ts
+++ stacks/linkedStack/linked-stack.spec.ts
@@ -8,28 +8,9 @@ describe('Linked Stack', () => {
     expect(sut.push('any')).toBe(undefined)
   })
 
-  test('Should method peek return undefined if no value are provided to Stack', () => {    
+  test('Should method peek return the last value if it was provided to push method', () => {    
     const sut = makeSut()
     expect(sut.peek()).toBe(undefined)
-  })
-
-  test('Should method pop return undefined if no value are provided to Stack', () => {    
-    const sut = makeSut()
-    expect(sut.pop()).toBe(undefined)
-  })
-
-  test('Should method size return zero if no value are provided to Stack', () => {    
-    const sut = makeSut()
-    expect(sut.size()).toBe(0)
-  })
-
-  test('Should method storage return a empty list if no value are provided to Stack', () => {    
-    const sut = makeSut()
-    expect(sut.storage()).toEqual([])
-  })
-
-  test('Should method peek return the last value provided in push method', () => {    
-    const sut = makeSut()
     sut.push('any')
     expect(sut.peek()).toBe('any')
     sut.push('other')
@@ -53,7 +34,7 @@ describe('Linked Stack', () => {
     expect(sut.pop()).toBe(undefined)
   })
 
-  test('Should method size return the exact amout of values provided in push method', () => {    
+  test('Should method size return the exact amout of values provided to push method', () => {    
     const sut = makeSut()
     for (let count = 0; count < 10; count ++) {
       expect(sut.size()).toBe(count)
@@ -61,11 +42,15 @@ describe('Linked Stack', () => {
     }
   })
 
-  test('Should method storage return a list with all values provided in push method, in same order that was pused', () => {
+  test('Should method storage return a list with all values provided to push method, in same order that was pushed', () => {
     const listToPush = ['any', 'other', 'another', 'another_one', 'other_one']
+    const listToCompare: unknown[] = []
     const sut = makeSut()
+    expect(sut.storage()).toEqual(listToCompare)
     for (const item of listToPush) {
       sut.push(item)
+      listToCompare.push(item)
+      expect(sut.storage()).toEqual(listToCompare)
     }
     expect(sut.storage()).toEqual(listToPush)
   })
