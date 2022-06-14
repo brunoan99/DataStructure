20e835173645f461a19c21077ea4e58a09b04572
Bruno Andrade <brunoan99@gmail.com>
2022-06-14 19:23:10 -0300

    refactor(SetOperations): substitute for using map instead

--- sets/setOperations/SetOperations.ts
+++ sets/setOperations/SetOperations.ts
@@ -6,22 +6,18 @@ import { SetClass } from '../set/Set'
 export class SetOperations implements ISetOperations {
   union (S: ISet<any>, T: ISet<any>): ISet<any> {
     const setToReturn = new SetClass()
-    for (const item of S.iterate().concat(T.iterate())) {
-      setToReturn.add(item)
-    } 
+    S.iterate().concat(T.iterate()).map(e => setToReturn.add(e))
     return setToReturn
   }
   intersection (S: ISet<any>, T: ISet<any>): ISet<any> {
     const setToReturn = new SetClass()
-    for (const itemS of S.iterate()) {
-      if (T.is_element_of(itemS)) {
-        setToReturn.add(itemS)
-      }
-    }
+    S.iterate().map(e => {
+      if(T.is_element_of(e)) setToReturn.add(e)
+    })
     return setToReturn
   }
   difference (S: ISet<any>, T: ISet<any>): ISet<any> {
-    throw new Error('Method not implemented.')
+    throw new Error('Method not implemented')
   }
   subset (S: ISet<any>, T: ISet<any>): ISet<any> {
     throw new Error('Method not implemented.')
