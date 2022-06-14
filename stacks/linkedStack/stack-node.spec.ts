5fa0295457f0e9017d4176359bfcd72cb0309f93
Bruno Andrade <brunoan99@gmail.com>
2022-06-14 13:16:03 -0300

    refactor(StackNode): refactor tests to remove next parameter from constructor

--- stacks/linkedStack/stack-node.spec.ts
+++ stacks/linkedStack/stack-node.spec.ts
@@ -2,30 +2,32 @@ import { StackNode } from './stackNode'
 
 describe('Stack Node', () => {
   test('Should StackNode by default point to null', () => {
-    const sut = new StackNode('any', null)
-    expect(sut.next).toBeNull()
+    const sut = new StackNode('any')
+    expect(sut.next).toBe(null)
   })
 
   test('Should StackNode store value provided', () => {
-    const sut = new StackNode('any', null)
+    const sut = new StackNode('any')
     expect(sut.value).toBe('any')
   })
 
   test('Should StackNode store a next StackNode when provided', () => {
-    const anotherStackNode = new StackNode('other_value', null)
-    const sut = new StackNode('any', anotherStackNode)
+    const anotherStackNode = new StackNode('other_value')
+    const sut = new StackNode('any')
+    sut.next = anotherStackNode
     expect(sut.next).toEqual(anotherStackNode)
-    expect(sut.next.value).toBe('other_value')
-    expect(sut.next.next).toBe(null)
+    expect(sut.next?.value).toBe('other_value')
+    expect(sut.next?.next).toBe(null)
   })
 
   test('Should StackNode be possible to overwrite a next StackNode', () => {
-    const overwriteStackNode = new StackNode('another_value', null)
-    const anotherStackNode = new StackNode('other_value', null)
-    const sut = new StackNode('any', anotherStackNode)
+    const overwriteStackNode = new StackNode('another_value')
+    const anotherStackNode = new StackNode('other_value')
+    const sut = new StackNode('any')
+    sut.next = anotherStackNode
     expect(sut.next).toEqual(anotherStackNode)
-    expect(sut.next.value).toBe('other_value')
-    expect(sut.next.next).toBe(null)
+    expect(sut.next?.value).toBe('other_value')
+    expect(sut.next?.next).toBe(null)
     sut.next = overwriteStackNode
     expect(sut.next).toEqual(overwriteStackNode)
     expect(sut.next.value).toBe('another_value')
