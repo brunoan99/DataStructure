a1daea64a6e32b4e2805881736332a05415b0d1b
Bruno Andrade <brunoan99@gmail.com>
2022-06-15 01:02:02 -0300

    feat(HashFunction): ensure hash return a number less than capacity parameter provided

--- hashTables/hashTable/hash-function.spec.ts
+++ hashTables/hashTable/hash-function.spec.ts
@@ -3,8 +3,9 @@ import { HashFunction } from './HashFunction'
 const makeSut = (): HashFunction => new HashFunction()
 
 describe('Hash Function', () => {
-  test('Should return a number', () => {
+  test('Should hash return a number less than capacity provided', () => {
     const sut = makeSut()
     expect(typeof sut.hash('any', 10)).toBe('number')
+    expect(sut.hash('any', 10)).toBeLessThan(10)
   })
 })
